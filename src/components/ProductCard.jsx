da6a096c51f87eb541f2e8f1975b53210ff053a8
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2022-03-27 18:35:10 +0530

    feat: add wishlist add functionality

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,35 +1,45 @@
 import React from "react";
+import { useWishListContext } from "../context";
+import { FavoriteIcon,FavoriteIconFilled } from "../icons/icons";
 
 export default function ProductCard({ productData }) {
+    const {id, title, description, price, imageSrc } = productData;
+    const {wishList,wishListHandler} = useWishListContext()
 
-    const {title,description,price,imageSrc} = productData
+    const isPresent = wishList.find(p => p.id === id)
 
-    const calculatePercentage = (price)=>{
-        const percentage = ((price.oldPrice - price.newPrice)*100)/price.oldPrice;
-        return Math.round(percentage);
+    const redColor = {
+        color: "var(--COLOR-ERROR-DARK)"
     }
+
+    const calculatePercentage = (price) => {
+        const percentage =
+            ((price.oldPrice - price.newPrice) * 100) / price.oldPrice;
+        return Math.round(percentage);
+    };
 
     return (
         <div className="card">
             <div className="card-img-wrapper">
-                <img
-                    className="card-img"
-                    src={imageSrc}
-                    alt="card-img"
-                />
+                <button className={`btn btn-icon btn-icon-red card-favorite`} style={isPresent?redColor:{}} onClick={()=>wishListHandler(productData)}>
+                    {isPresent?<FavoriteIconFilled/>:<FavoriteIcon/>}
+                </button>
+                <img className="card-img" src={imageSrc} alt="card-img" />
             </div>
             <div className="card-content-wrapper">
                 <div className="card-content">
                     <h2 className="card-title">{title}</h2>
-                    <div className="card-description">
-                        {description}
-                    </div>
+                    <div className="card-description">{description}</div>
                 </div>
                 <div className="card-cta">
                     <div className="card-price">
-                        <span className="card-discount-price">Rs {price.newPrice}</span>
+                        <span className="card-discount-price">
+                            Rs {price.newPrice}
+                        </span>
                         <div className="card-price-details">
-                            <span className="card-original-price">Rs {price.oldPrice}</span>
+                            <span className="card-original-price">
+                                Rs {price.oldPrice}
+                            </span>
                             <span className="card-discount-percentage">
                                 {calculatePercentage(price)}% off
                             </span>
@@ -37,10 +47,10 @@ export default function ProductCard({ productData }) {
                     </div>
                     <div className="card-buttons flex-stretch">
                         <button className="btn btn-default card-btn">
-                            Add to Cart
+                            Buy Now
                         </button>
                         <button className="btn btn-outline card-btn ">
-                            WishList
+                            Add to Cart
                         </button>
                     </div>
                 </div>
