ce32c6a3a82346c9e290b35752ac5e8f4140d614
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2022-03-27 16:28:08 +0530

    refactor: icons

--- src/icons/icons.jsx
+++ src/icons/icons.jsx
@@ -2,33 +2,25 @@ import React from 'react'
 
 export default function ShoppingCartIcon() {
   return (
-    <div className="badge-container flex flex-column">
         <span className="material-icons font-x-large">shopping_cart</span>
-    </div>
   )
 }
 
 export function AccountIcon() {
     return (
-    <div className="badge-container flex flex-column">
         <span className="material-icons font-x-large">account_circle</span>
-    </div>
     )
   }
   
 export function WishListIcon() {
     return (
-      <div className="badge-container flex flex-column">
           <span className="material-icons font-x-large">favorite_border</span>
-      </div>
     )
   }
 
 export function DarkModeIcon() {
   return (
-    <div className="badge-container flex flex-column">
         <span className="material-icons font-x-large">dark_mode</span>
-    </div>
   )
 }
 
