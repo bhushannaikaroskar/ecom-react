1685cdc8a8dcc56c74ef596d0e8ed873e6a93088
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2022-03-29 00:22:03 +0530

    update: add cart items display

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { useWishListContext } from "../../context";
+import { useCartContext, useWishListContext } from "../../context";
 import ShoppingCartIcon, {
     AccountIcon,
     DarkModeIcon,
@@ -10,8 +10,8 @@ import Logo from "./logo";
 import SearchBar from "./searchbar";
 
 export default function NavBar() {
-
-    const {wishList} = useWishListContext();
+    const { wishList } = useWishListContext();
+    const { cart } = useCartContext();
     return (
         <nav className="grand-nav navbar navbar-responsive box-shadow-100 p-2_5 p-y-1">
             <Logo />
@@ -21,16 +21,23 @@ export default function NavBar() {
                     className="btn btn-link-secondary justify-content-start font-color-gray"
                     to="/wishlist"
                 >
-                    <div class="badge-container flex flex-column">
+                    <div className="badge-container flex flex-column">
                         <WishListIcon />
-                        {wishList.length>0 && <span class="badge">{wishList.length}</span>}
+                        {wishList.length > 0 && (
+                            <span className="badge">{wishList.length}</span>
+                        )}
                     </div>
                 </NavLink>
                 <NavLink
                     className="btn btn-link-secondary justify-content-start font-color-gray"
                     to="/cart"
                 >
-                    <ShoppingCartIcon />
+                    <div className="badge-container flex flex-column">
+                        <ShoppingCartIcon />
+                        {cart.length > 0 && (
+                            <span className="badge">{cart.length}</span>
+                        )}
+                    </div>
                 </NavLink>
                 <NavLink
                     className="btn btn-link-secondary justify-content-start font-color-gray"
