5355ff9302fe8bf5c5de8682ce4210bab3ef12ac
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2022-03-31 06:43:14 +0530

    add: mock backend

---
+++ src/backend/db/products.js
@@ -0,0 +1,52 @@
+import { v4 as uuid } from "uuid";
+
+/**
+ * Product Database can be added here.
+ * You can add products of your wish with different attributes
+ * */
+
+export const products = [
+  {
+    _id: 1,
+    title: "You Can WIN",
+    author: "Shiv Khera",
+    price: "5000",
+    categoryName: "non-fiction",
+  },
+  {
+    _id: 2,
+    title: "You are Winner",
+    author: "Junaid Qureshi",
+    price: "3000",
+    categoryName: "horror",
+  },
+  {
+    _id: 2,
+    title: "Think and Grow Rich",
+    author: "Shiv Khera",
+    price: "1000",
+    categoryName: "fiction",
+  },
+];
+
+// {
+//   _id: uuid(),
+//   title: "You Can WIN",
+//   author: "Shiv Khera",
+//   price: "5000",
+//   categoryName: "non-fiction",
+// },
+// {
+//   _id: uuid(),
+//   title: "You are Winner",
+//   author: "Junaid Qureshi",
+//   price: "3000",
+//   categoryName: "horror",
+// },
+// {
+//   _id: uuid(),
+//   title: "Think and Grow Rich",
+//   author: "Shiv Khera",
+//   price: "1000",
+//   categoryName: "fiction",
+// },
