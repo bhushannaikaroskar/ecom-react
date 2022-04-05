3b8cd9ec4939d9854af353dba3919568a4917dd7
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2022-04-05 20:39:42 +0530

    add: custom useAxios hook

---
+++ src/utils/custom-hooks/useAxios.jsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default function useAxios() {
+    const [response, setResponse] = useState();
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const fetchData = async (params) => {
+        let isError = false;
+        let errorData = {}
+        let result1 = {}
+        setLoading(true)
+        try {
+            const result = await axios.request(params);
+            result1 = result
+            setResponse(result);
+            setError("")
+
+        } catch (err) {
+            console.log(err.response)
+            isError=true;
+            errorData = err;
+            setResponse("");
+            setError(err);
+        } finally {
+            setLoading(false);
+        }
+
+        return isError?errorData.response:result1
+    };
+
+    return { response, error, loading, fetchData };
+}
