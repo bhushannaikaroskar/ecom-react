dce58ab44053d09e72a2d81788331e100f626f5d
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2022-04-06 02:14:41 +0530

    refactor: remove commented code

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -101,25 +101,3 @@ export const products = [
         rating: 3,
     },
 ];
-
-// {
-//   _id: uuid(),
-//   title: "You Can WIN",
-//   author: "Shiv Khera",
-//   price: "5000",
-//   categoryName: "non-fiction",
-// },
-// {
-//   _id: uuid(),
-//   title: "You are Winner",
-//   author: "Junaid Qureshi",
-//   price: "3000",
-//   categoryName: "horror",
-// },
-// {
-//   _id: uuid(),
-//   title: "Think and Grow Rich",
-//   author: "Shiv Khera",
-//   price: "1000",
-//   categoryName: "fiction",
-// },
