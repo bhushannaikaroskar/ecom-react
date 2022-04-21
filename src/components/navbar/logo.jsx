d20f7598232eede1723f9f1aadf568782ab16cb6
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2022-04-21 15:14:14 +0530

    add: search feature

--- src/components/navbar/logo.jsx
+++ src/components/navbar/logo.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { grandDarkLogo, grandLogo } from '../../assets'
-import { useTheme } from '../../context'
+import { useProductContext, useTheme } from '../../context'
 
 export default function Logo() {
 
   const {theme} = useTheme();
+  const {setSearchValue} = useProductContext()
 
   return (
-    <Link to="/" className="nav-logo">
+    <Link to="/" className="nav-logo"
+    onClick={()=>setSearchValue("")}>
         <img src={theme==="light"?grandLogo:grandDarkLogo} alt="logo"/>
     </Link>
   )
--- src/components/navbar/searchbar.jsx
+++ src/components/navbar/searchbar.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { useProductContext } from "../../context";
 import { SearchIcon } from "../../icons/icons";
 
 export default function SearchBar() {
+
+    const [value,setValue] = useState("")
+    const {setSearchValue} = useProductContext()
+
+    const searchHandler = (e) =>{
+        if(e.code === "Enter"){
+            console.log(value)
+            setSearchValue(value)
+            setValue("")
+        }
+    }
+
     return (
-        <div className="nav-search-container">
-            <SearchIcon/>
+        <div
+            className="nav-search-container"
+        >
+            <SearchIcon />
             <input
+                style={{ width: "100%" }}
                 type="search"
+                value={value}
                 className="input-field nav-search input-rounded"
                 placeholder="Search"
+                onChange={(e)=>setValue(e.target.value)}
+                onKeyDownCapture={searchHandler}
             />
         </div>
     );
